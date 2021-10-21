4ca491dc1208c7c5cbeabbc0a652c8663604085f
i.gura <gura.ilya2011@yandex.ru>
2021-10-21 21:34:47 +0300

    init

---
+++ app.php
@@ -0,0 +1,9 @@
+<?php
+require_once __DIR__ . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+
+$app = new \Symfony\Component\Console\Application('name console');
+
+$app->add(new \App\WhatName());
+
+$app->run();
+
